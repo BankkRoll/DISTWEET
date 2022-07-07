9df9f07cafba53dba53407a7f2d56118943090f2
BankkRoll <106103625+BankkRoll@users.noreply.github.com>
2022-07-07 16:26:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -42,17 +42,23 @@ discord_channel_id2='ENTER CHANNEL ID2'
 discord_channel_id3='ENTER CHANNEL ID3'
 
 
-#This wil make sure out bot is online and ready , as well as set a activity presence
+
+# This will output when bot is online , as well as set a activity presence
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for tweets to post"))
     print(f'{client.user} has connected to Discord!')
     sleep(0.5)
-    print('Bot is ready!')
+    print("A list of all servers where the bot is on:")
+    # List of all Servers the Bot is
+    insguilds = 0
+    for guild in client.guilds:
+      print("%s - %s" % (guild.name, guild.id))
+      insguilds = insguilds + 1
+      print("Guilds: " + str(insguilds))
 
-
-#This is the action to take
-#Watch for messages and post tweet upon a message post.
+# This is the action to take
+# Watch for messages and post tweet upon a message post.
 @client.event
 async def on_message(message):
     if message.author != client.user and str(message.channel.id) ==discord_channel_id1 :
