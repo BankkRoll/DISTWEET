b0e1b9d4949a6473fc0348a28a971a5613d2752b
BankkRoll <106103625+BankkRoll@users.noreply.github.com>
2022-07-31 23:17:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,80 +4,70 @@ from time import sleep
 import asyncio
 import tweepy
 
-
-#This is your discord bot token DO NOT SHARE!!
+# Setup Discord bot
 discord_token = os.environ['discord_token']
+client=discord.Client()
 
-#Here is where we will store information for our twitter developer api keys
-#this will be twitter bot 1
-access_token_secret1 = os.environ['access_token_secret1']
-access_token1 = os.environ['access_token1']
-consumer_secret1 = os.environ['consumer_secret1']
-consumer_key1 = os.environ['consumer_key1']
+#here is where you will put information for your twitter developer api keys
+access_token_secret1 = os.environ['access_token_secret']
+access_token1 = os.environ['access_token']
+consumer_secret1 = os.environ['consumer_secret']
+consumer_key1 = os.environ['consumer_key']
 
-#Here is where we will store information for our twitter developer api keys
-#this will be twitter bot 2
-access_token_secret2 = os.environ['access_token_secret2']
-access_token2 = os.environ['access_token2']
-consumer_secret2 = os.environ['consumer_secret2']
-consumer_key2 = os.environ['consumer_key2']
 
-#Here is where we will store information for our twitter developer api keys
-#this will be twitter bot 3
-access_token_secret3 = os.environ['access_token_secret3']
-access_token3 = os.environ['access_token3']
-consumer_secret3 = os.environ['consumer_secret3']
-consumer_key3 = os.environ['consumer_key3']
-
-#This is where we we auth twitter api keys via tweepy
+# Initiate Tweepy + Discord APIs
 auth=tweepy.OAuthHandler(consumer_key1,consumer_secret1)
 auth.set_access_token(access_token1,access_token_secret1)
 api = tweepy.API(auth)
 
-client=discord.Client()
-
-#here are your channels ids for each bot
-discord_channel_id1='ENTER CHANNEL ID1'
-discord_channel_id2='ENTER CHANNEL ID2'
-discord_channel_id3='ENTER CHANNEL ID3'
+# Channel IDs
+discord_channel_id1='000000000000000000'
+discord_channel_id2='000000000000000000'
+discord_channel_id3='000000000000000000'
 
 
 
-# This will output when bot is online , as well as set a activity presence
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="for tweets to post"))
-    print(f'{client.user} has connected to Discord!')
+    print('We have logged in as {0.user}'.format(client))
     sleep(0.5)
     print("A list of all servers where the bot is on:")
     # List of all Servers the Bot is
     insguilds = 0
     for guild in client.guilds:
-      print("%s - %s" % (guild.name, guild.id))
+      print(f"{guild.name} - {guild.id}")
       insguilds = insguilds + 1
       print("Guilds: " + str(insguilds))
 
-# This is the action to take
-# Watch for messages and post tweet upon a message post.
 @client.event
 async def on_message(message):
-    if message.author != client.user and str(message.channel.id) ==discord_channel_id1 :
-        api.update_status(status=message.content)
+    # Ignore messages from self
+    if message.author == client.user:
+        return
+    # Extract needed variables
+    channel = str(message.channel.id)
+    # Channel ID 1
+    if channel == discord_channel_id1:
+        api.update_status(
+            message.embeds[0].description + "\n" + message.embeds[0].image.url
+        )
         print('Tweet posted from channel 1')
-    
-    if message.author != client.user and str(message.channel.id) ==discord_channel_id2 :
-        api.update_status(status=message.content)
+    # Channel ID 2
+    if channel ==discord_channel_id2:
+        api.update_status(
+            message.embeds[0].description + "\n" + message.embeds[0].image.url
+        )
         print('Tweet posted from channel 2')
-    
-    if message.author != client.user and str(message.channel.id) ==discord_channel_id3 :
-        api.update_status(status=message.content)
+    # Channel ID 3
+    if channel ==discord_channel_id3:
+        api.update_status(
+            message.embeds[0].description + "\n" + message.embeds[0].image.url
+        )
         print('Tweet posted from channel 3')
-    
 
-
-
+# Initiate bot
 async def main_func():
     await client.start(discord_token)
 
-
-asyncio.get_event_loop().run_until_complete(main_func())
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main_func())
